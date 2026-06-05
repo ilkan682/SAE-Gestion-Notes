3b4f52c444d6271d5203821bf6f81410663b95f4
hussainshaqsi <hussainshaqsi18@gmail.com>
2026-06-05 14:41:59 +0200

    Ressources fix

--- notes_app/urls_ressources.py
+++ notes_app/urls_ressources.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('ressources/', views.ressource_liste, name='ressource_liste'),
     path('ressources/ajouter/', views.ressource_creer, name='ressource_create'),
-    path('ressources/<int:pk>/', views.ressource_detail, name='ressource_detail'),
-    path('ressources/<int:pk>/modifier/', views.ressource_modifier, name='ressource_update'),
-    path('ressources/<int:pk>/supprimer/', views.ressource_supprimer, name='ressource_delete'),
+    path('ressources/<str:pk>/', views.ressource_detail, name='ressource_detail'),
+    path('ressources/<str:pk>/modifier/', views.ressource_modifier, name='ressource_update'),
+    path('ressources/<str:pk>/supprimer/', views.ressource_supprimer, name='ressource_delete'),
 ]
