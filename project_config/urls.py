cef8b30d30ce614cbc7f12fca29e6073592734ec
Ilkan Guney <g682ilkan@gmail.com>
2026-06-12 20:56:20 +0200

    Update urls.py

--- project_config/urls.py
+++ project_config/urls.py
@@ -1,6 +1,4 @@
-"""
-URLs principales du projet SAÉ Gestion des Notes
-"""
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -18,11 +16,4 @@ urlpatterns = [
     path('', include('notes_app.urls_notes')),
     path('import/', include('notes_app.urls_import')),
     path('releve/', include('notes_app.urls_releve')),
-    # Les URL des autres branches seront ajoutées ici lors de la fusion
-    # path('ressources/', include('notes_app.urls_ressources')),   # dev-hussain
-    # path('examens/', include('notes_app.urls_examens')),         # dev-hussain
-    # path('notes/', include('notes_app.urls_notes')),             # dev-hussain + dev-rafael
-    # path('import/', include('notes_app.urls_import')),           # dev-rafael
-    # path('releve/', include('notes_app.urls_releve')),           # dev-rafael
-    # path('import/', include('notes_app.urls_import')),   # dev-rafael
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
